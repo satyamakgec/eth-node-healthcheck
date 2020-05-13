95686e1f425dc738281261c9d6ce267df41ceb15
satyam <satyam@secureblocks.io>
2020-05-13 05:57:58 +0530

    change ethers to web3

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-const ethers = require('ethers');
+const Web3 = require('web3');
 const http = require('http');
 
 const host = process.env.RPC_HOST || 'localhost';
-const provider = ethers.getDefaultProvider(process.env.NETWORK);
-const localProvider = new ethers.providers.JsonRpcProvider(`http://${host}:8545`);
+const etherscan_web3 = new Web3(new Web3.providers.HttpProvider(process.env.CONNECTION_URL));
+const local_web3 = new Web3(new Web3.providers.HttpProvider(`http://${host}:8545`));
 const MAX_BLOCK_DIFFERENCE = 3;
 
 const onHealthcheckRequest = async (req, res) => {
@@ -14,17 +14,20 @@ const onHealthcheckRequest = async (req, res) => {
 
   let localBlockNum;
   let networkBlockNum;
+  let syncStatus;
 
   try {
-    localBlockNum = await localProvider.getBlockNumber();
-    networkBlockNum = await provider.getBlockNumber();
+    localBlockNum = await local_web3.eth.getBlockNumber();
+    networkBlockNum = await etherscan_web3.eth.getBlockNumber();
+    syncStatus = typeof await local_web3.eth.isSyncing() == Object ? true : false;
   } catch (e) {
     console.error(e);
     res.writeHead(500, { 'Content-Type': 'text/plain' });
     res.end(e);
   }
 
-  let responseStatus = networkBlockNum - localBlockNum > MAX_BLOCK_DIFFERENCE ? 500 : 200;
+
+  let responseStatus = (networkBlockNum - localBlockNum > MAX_BLOCK_DIFFERENCE || syncStatus) ? 500 : 200;
   if (localBlockNum > 10000 && networkBlockNum <= 0) { // don't let etherscan f**k us
     responseStatus = 200;
   }
